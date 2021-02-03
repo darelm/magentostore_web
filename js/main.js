c296df934445798d90c98bd7e5858c86ad34180b
Saleh El-Jabarty <saleh.awal@gmail.com>
2021-02-03 03:12:15 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    const floatingBtn = $('.floating-btn');
+    let windowHeight = $(window).height();
+
+    $(window).on('scroll', (e) => {
+        if (windowHeight < $(window).scrollTop()) {
+            floatingBtn.removeClass('hidden')
+        } else {
+            if (!floatingBtn.hasClass('hidden')) {
+                floatingBtn.addClass('hidden')
+            }
+        }
+
+
+    });
+
+    floatingBtn.on('click', (e) => {
+        $("html, body").animate({scrollTop: 0}, 500);
+    })
+});
