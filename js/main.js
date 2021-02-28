934438d6d364bedab8493e4cff3e508d17995a46
Saleh El-Jabarty <saleh.awal@gmail.com>
2021-02-28 18:29:22 +0200

    added instagram posts images

--- js/main.js
+++ js/main.js
@@ -24,3 +24,13 @@ function getURL(urlstr)
 {
     window.open(urlstr, '_blank');
 }
+
+function load_iframe(iframeid,urlstr)
+{
+    Cookies.set('name', 'value', {sameSite: 'None', secure: true});
+    /*var url_string = urlstr;
+    var url = new URL(url_string);
+    var c = url.searchParams.get("url");
+    document.getElementById(iframeid).src = c;*/
+    document.getElementById(iframeid).src = 'http://magento2.magentech.com/themes/sm_moonlight/default/';
+}
